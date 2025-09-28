5e4553167e2dedf93d1895b5b08d3823ecde86ea
Srushti Milind Chavan <srushti.m.chavan@gmail.com>
2025-09-28 12:13:05 +0530

    Add files via upload

---
+++ java1/src/javabasics/abc.java
@@ -0,0 +1,25 @@
+package javabasics;
+
+public class abc {
+    static void main(String[] args)
+    {
+        System.out.println("this is lavanya first prorgam in my laptop of dear darling babay shrsuti ");
+        System.out.println(getnum());
+       abc ab= new abc();
+      ab.  method();
+
+    }
+    //no need of object to use this method
+ public static  int getnum()
+ {
+     int a=10;
+     return 10*10;
+ }
+ //non static method
+    //need object to use this method in main
+ public void method()
+
+ {
+     System.out.println("my method");
+ }
+}
